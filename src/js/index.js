0314cd83ab19bf55b727b9f2ccfd4cbb4477f652
Hong Baojin <13809735212@163.com>
2019-11-08 18:41:22 +0800

    add test.html

--- src/js/index.js
+++ src/js/index.js
@@ -1,39 +1,63 @@
 $(function () {
   /* 顶部导航菜单 */
-  function headerNavMenu() {
-    $(".nav-list li").hover(function () {
-      $(this).find(".ns-menu").stop(true, false).show();
-    }, function () {
-      $(this).find(".ns-menu").stop(true, false).hide();
+  function headerNavMenu(headEle, parentEle, contEle) {
+    $(headEle).hover(function(){ // 移入
+      var $this = $(this);
+      if ($(contEle, $this).size() >= 1 && $(contEle, $this).hasClass('hide')) {
+        $(contEle, $this).show();
+      }
+    },function(){ // 父亲 li 移除
+      var $this = $(this);
+      if ($(contEle, $this).size() >= 1 && $(contEle, $this).hasClass('hide')) {
+        $(contEle, $this).hide();
+      }
     });
   }
-  headerNavMenu();
+  // headerNavMenu('.nav-list > li', null, '.ns-menu');
+  
+  /* 鼠标移入切换 */
+  function mouseenterTab(headEle, parentEle, contEle, className) {
+    $(headEle).mouseenter(function () {
+      var $this = $(this);
+      $this.parent().parent().find('li').removeClass(className);
+      $this.addClass(className);
+      $this.parents(parentEle).find(contEle).removeClass(className);
+      $this.parents(parentEle).find(contEle).eq($this.index()).addClass(className);
+    });
+  }
+  // 建筑类考试
+  mouseenterTab(".box1-t .list li", ".qh", ".ul", "active");
+  // 精选考点
+  mouseenterTab(".box1-c2 .list li", ".box1-c2", ".qhlist", "active");
+  mouseenterTab(".box5 .list li", ".qh", ".c", "active");
+  mouseenterTab(".ns-footer-nav .tab-head li", ".ns-footer-nav", ".cont-item", "cur");
+  // 直播-列表页面 科目
+  mouseenterTab(".ns-live-list .list-km li", null, null, "cur");
 
-  $(".box1-t .list li").mouseenter(function () {
-    var $this = $(this);
-    $this.parent().parent().find('li').removeClass("active");
-    $this.addClass("active");
-    // $this.parents('.qh').find('.c').removeClass("active");
-    // $this.parents('.qh').find('.c').eq( $this.index() ).addClass("active");
-    $this.parents('.qh').find('.ul').removeClass("cur");
-    $this.parents('.qh').find('.ul').eq($this.index()).addClass("cur");
+  // 多选框
+  function checkboxHandle(aimEle, className){
+    $(aimEle).click(function(){
+      $(this).toggleClass(className);
+    });
+  }
+  checkboxHandle('.zt-cont li', 'sel');
+  checkboxHandle('.l-checkbox li', 'active');
+  checkboxHandle('.zs-fd .row-xk span', 'sel');
+  checkboxHandle('.detail-checkbox span', 'sel');
+  // 提交答案
+  $('#submit').click(function(){
+    // console.log('选择的答案：', $('.zt-cont li.sel').find('span').text());
+    $('#answer').val($('.zt-cont li.sel').find('span').text());
   });
 
-  $(".box1-c2 .list li").mouseenter(function () {
-    var $this = $(this);
-    $this.parent().parent().find('li').removeClass("active");
-    $this.addClass("active");
-    $this.parents('.box1-c2').find('.qhlist').removeClass("active");
-    $this.parents('.box1-c2').find('.qhlist').eq($this.index()).addClass("active");
-  });
-
-  $(".box5 .list li").click(function () {
-    var $this = $(this);
-    $this.parent().parent().find('li').removeClass("active");
-    $this.addClass("active");
-    $this.parents('.qh').find('.c').removeClass("active");
-    $this.parents('.qh').find('.c').eq($this.index()).addClass("active");
-  });
+  // 单选框
+  function radioHandle(aimEle, className){
+    $(aimEle).click(function(){
+      $(this).addClass(className).siblings().removeClass(className);
+    });
+  }
+  radioHandle('.l-radio li', 'active');
+  radioHandle('.detail-radio span', 'sel');
 
   $(".jt").click(function () {
     $(this).toggleClass("active");
@@ -54,17 +78,17 @@ $(function () {
     $(togBox).each(function () {
       $(this).find(tagBox).children().eq(0).addClass(addclasses);
       $(this).find(conBox).children().eq(0).show().siblings().hide()
-      $(this).find(tagBox).children().click(function () {
+      $(this).find(tagBox).children().mouseenter(function () {
         var index = $(this).index();
         $(this).addClass(addclasses).siblings().removeClass(addclasses);
         $(this).parents(togBox).find(conBox).children().eq(index).show().siblings().hide();
       })
     })
   }
-  toggleTag('.toggleMaxBox', '.toggleTag', '.toggleShowBox', 'active')
-  toggleTag('.toggleMaxBox1', '.toggleTag1', '.toggleShowBox1', 'active')
+  toggleTag('.toggleMaxBox', '.toggleTag', '.toggleShowBox', 'active');
+  toggleTag('.toggleMaxBox1', '.toggleTag1', '.toggleShowBox1', 'active');
 
-  //文章折叠
+  /* 文章折叠 */
   function clickFold() {
     $(".foldbox").eq(0).find("ul").hide();
     $(".foldbox").eq(0).find(".tag-title").addClass("active")
@@ -76,4 +100,56 @@ $(function () {
     })
   }
   clickFold();
+
+  /* SuperSlide */
+  if ($(".teacher-slide").length) { // 过滤没有SuperSlide页面
+    $(".teacher-slide").slide({
+      mainCell: ".bd ul",
+      effect: "leftLoop"
+    });
+  }
+
+  // 搜索框失去焦点
+  // $('#text_one').focus(function(){
+  //   $('#nsHot').show();
+  // });
+  // 热门搜索推荐
+  // $('#nsHot li a').click(function(){
+  //   $('#text_one').val($(this).text());
+  //   $(this).parents('#nsHot').hide();
+  // });
+});
+/* 搜索框 */
+function CheckForm() {
+  if (document.search_one.text_one.value == "") {
+    alert("请输入需要搜索的关键词");
+    document.search_one.text_one.focus();
+    return false;
+  } else {
+    var urlstr = encodeURI(document.search_one.text_one.value);
+    //var city = document.search_one.citymk.value;
+    var urlx = 'http://zhannei.baidu.com/cse/search?s=6924920297305690263&entry=1&q=' + urlstr;
+    //document.search_one.action = urlx;
+    //document.search_one.submit();
+    window.open(urlx);
+  }
+  return false
+}
+
+$(function(){
+  /* 顶部导航菜单 */
+  function topbar(headEle, contEle) {
+    $(headEle).hover(function(){ // 移入
+      var $this = $(this);
+      if ($(contEle, $this).size() >= 1 && $(contEle, $this).hasClass('hide')) {
+        $(contEle, $this).show();
+      }
+    },function(){ // 父亲 li 移除
+      var $this = $(this);
+      // if ($(contEle, $this).size() >= 1 && $(contEle, $this).hasClass('hide')) {
+      //   $(contEle, $this).hide();
+      // }
+    });
+  }
+  topbar('.nav-list > li', '.ns-menu')
 });
